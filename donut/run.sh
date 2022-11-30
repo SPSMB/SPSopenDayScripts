3c49c19f610697880f200497610b34f34f12c496
CrimsonBaron <thecrimsonfucker666@seznam.cz>
2022-11-30 21:09:16 +0100

    update

--- donut/run.sh
+++ donut/run.sh
@@ -6,6 +6,8 @@ apt install python3
 DONUT=./donut.py
 INIT=./init.txt
 
+echo "credit(for donut code): https://github.com/limiteci/donut.c"
+
 if [[ ! -f "$DONUT" && -f "$INIT" ]]; then
     cat init.txt > donut.py
 fi
