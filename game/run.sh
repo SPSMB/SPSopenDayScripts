19087aec9bb690d1376996d9ff36bea8eb2f898e
CrimsonBaron <thecrimsonfucker666@seznam.cz>
2022-11-30 20:42:10 +0100

    update

--- game/run.sh
+++ game/run.sh
@@ -13,27 +13,27 @@ do
     read -p num
     if [$NUM -eq 1]; then
         cat ./frame/gameScreen.txt
-        read -p NUM
+        read NUM
     fi
 
     if [$NUM1 -eq 1]; then
         cat ./frame/gameScreen.txt
-        read -p NUM1
+        read NUM1
     fi
 
     if [$NUM2 -eq 1]; then
         cat ./frame/gameScreen2.txt
-        read -p NUM2
+        read NUM2
     fi
 
     if [$NUM3 -eq 1]; then
         cat ./frame/gameScreen3.txt
-        read -p NUM3
+        read NUM3
     fi
 
     if [$NUM4 -eq 1]; then
         cat ./frame/gameScreen4.txt
-        read -p NUM4
+        read NUM4
     fi
 done
 
