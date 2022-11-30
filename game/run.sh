b7112ced8bd36de4815d9aae432f33655cbdaf4c
CrimsonBaron <thecrimsonfucker666@seznam.cz>
2022-11-30 21:01:21 +0100

    update

--- game/run.sh
+++ game/run.sh
@@ -5,34 +5,35 @@ while :
 do
     clear
     cat ./frame/gameStart.txt
-    read -p "\n enter: " NUM
+    read -p "enter: " NUM
     if [ $NUM -eq 1 ]; then
         clear
         cat ./frame/gameScreen.txt 
     fi
 
-    read -p "\n enter: " NUM1
+    read -p "enter: " NUM1
     if [ $NUM1 -eq 1 ]; then
         clear
         cat ./frame/gameScreen.txt
     fi
 
-    read -p "\n enter: " NUM2
+    read -p "enter: " NUM2
     if [ $NUM2 -eq 1 ]; then
         clear
         cat ./frame/gameScreen2.txt
     fi
 
-    read -p"\n enter: " NUM3
+    read -p"enter: " NUM3
     if [ $NUM3 -eq 1 ]; then
         clear
         cat ./frame/gameScreen3.txt
     fi
 
-    read -p "\n enter: " NUM4
+    read -p "enter: " NUM4
     if [ $NUM4 -eq 1 ]; then
         clear
         cat ./frame/gameScreen4.txt
+        read
     fi
 done
 
