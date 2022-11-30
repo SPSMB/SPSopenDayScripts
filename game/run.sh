67e3dab2385bd192b79b5f2724e81b6af998ca84
CrimsonBaron <thecrimsonfucker666@seznam.cz>
2022-11-30 20:37:21 +0100

    update

--- game/run.sh
+++ game/run.sh
@@ -2,6 +2,10 @@
 clear
 
 NUM=0
+NUM1=0
+NUM2=0
+NUM3=0
+NUM4=0
 
 while :
 do
@@ -9,32 +13,27 @@ do
     read -p num
     if [$NUM -eq 1]; then
         cat ./frame/gameScreen.txt
-        $NUM = 0
         read -p NUM
     fi
 
-    if [$NUM -eq 1]; then
+    if [$NUM1 -eq 1]; then
         cat ./frame/gameScreen.txt
-          $NUM = 0
-        read -p NUM
+        read -p NUM1
     fi
 
-    if [$NUM -eq 1]; then
+    if [$NUM2 -eq 1]; then
         cat ./frame/gameScreen2.txt
-          $NUM = 0
-        read -p NUM
+        read -p NUM2
     fi
 
-    if [$NUM -eq 1]; then
+    if [$NUM3 -eq 1]; then
         cat ./frame/gameScreen3.txt
-          $NUM = 0
-        read -p NUM
+        read -p NUM3
     fi
 
-    if [$NUM -eq 1]; then
+    if [$NUM4 -eq 1]; then
         cat ./frame/gameScreen4.txt
-          $NUM = 0
-        read -p NUM
+        read -p NUM4
     fi
 done
 
