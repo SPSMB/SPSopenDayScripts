94a9322981ea200e77ce92e5fda3d0ee5eff2bb5
CrimsonBaron <thecrimsonfucker666@seznam.cz>
2022-11-30 20:28:25 +0100

    update

---
+++ game/run.sh
@@ -0,0 +1,41 @@
+#! /bin/bash
+clear
+
+NUM=0
+
+while :
+do
+    cat ./frame/gameStart.txt
+    read -p num
+    if [$NUM -eq 1]; then
+        cat ./frame/gameScreen.txt
+        NUM = 0
+        read -p NUM
+    fi
+
+    if [$NUM -eq 1]; then
+        cat ./frame/gameScreen.txt
+          NUM = 0
+        read -p NUM
+    fi
+
+    if [$NUM -eq 1]; then
+        cat ./frame/gameScreen2.txt
+          NUM = 0
+        read -p NUM
+    fi
+
+    if [$NUM -eq 1]; then
+        cat ./frame/gameScreen3.txt
+          NUM = 0
+        read -p NUM
+    fi
+
+    if [$NUM -eq 1]; then
+        cat ./frame/gameScreen4.txt
+          NUM = 0
+        read -p NUM
+    fi
+done
+
+
